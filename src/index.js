c73e315391d94b1cd18f81674b7ff3513c4b1610
marina-nakagiri <84775885+marina-nakagiri@users.noreply.github.com>
2022-07-04 17:18:32 +0900

    [Mod] js途中まで

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./styles.css";
+
+//onClickAddという箱で何かアクションすると、アラートを出します。
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化します。
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  //pタグ
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  div.appendChild(p);
+  console.log(div);
+};
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
